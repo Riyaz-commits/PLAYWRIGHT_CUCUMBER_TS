6632f991d036a16945650d5f3840394eda223ab8
Riyaz Basha Shaik <73514287+Riyaz-commits@users.noreply.github.com>
2024-08-22 17:44:10 +0530

    Changes on git hub actions file v2

--- src/helper/util/logger.ts
+++ src/helper/util/logger.ts
@@ -1,11 +1,26 @@
 import { transports , format} from 'winston';
+import * as path from 'path';
+import * as fs from  "fs-extra"
 
+// Function to ensure a directory exists
+const ensureDirectoryExistence = (dirPath: string) => {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+};
 
-export function options(scenarioName: string){
+// Check and create 'test-results' directory
+const testResultsDirectory = 'test-results';
+ensureDirectoryExistence(testResultsDirectory);
+
+// Example usage of the directory in your options function
+export function options(scenarioName: string) {
+  const logDirectory = path.join(testResultsDirectory, `logs/${scenarioName}`);
+  ensureDirectoryExistence(logDirectory);
     return{
   transports: [
       new transports.File({
-          filename: `test-tesults/logs/${scenarioName}/log.log`,
+         filename: path.join(logDirectory, 'log.log'),
           level: 'info',
          format: format.combine(
             format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss'}),
